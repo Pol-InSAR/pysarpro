1d636f8a33665603dbeadd2443a3580f94f6a9bf
Islam Mansour <is3mansour@gmail.com>
2024-01-19 10:04:37 +0100

    update failing test

--- pysarpro/data/__init__.pyi
+++ pysarpro/data/__init__.pyi
@@ -1,25 +1,9 @@
 __all__ = [
     'astronaut',
     'data_dir',
+    'download_all',
+    'file_hash',
 ]
 
 from ._binary_blobs import binary_blobs
-from ._fetchers import (
-    astronaut,
-    brain,
-    brick,
-    camera,
-    cat,
-    cell,
-    cells3d,
-    checkerboard,
-    chelsea,
-    clock,
-    coffee,
-    coins,
-    colorwheel,
-    data_dir,
-    download_all,
-    eagle,
-    file_hash,
-)
+from ._fetchers import astronaut, data_dir, download_all, file_hash
