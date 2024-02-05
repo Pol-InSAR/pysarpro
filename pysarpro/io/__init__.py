c2cdf60500a4655efd628b23bfe2e1dbd0ca2965
Islam Mansour <is3mansour@gmail.com>
2024-02-05 16:30:52 +0100

    edit rat reader

--- pysarpro/io/__init__.py
+++ pysarpro/io/__init__.py
@@ -4,13 +4,12 @@ The following plug-ins are available:
 
 """
 
+from ._image_stack import *
+from ._io import *
+from .collection import *
 from .manage_plugins import *
 from .sift import *
-from .collection import *
-
-from ._io import *
-from ._image_stack import *
-
+from .ste import *
 
 reset_plugins()
 
